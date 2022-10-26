696c4ac31da6a0e2550d69ef1adab3894281315f
Mateo Álvarez <malvarez@prexcard.com>
2022-10-26 14:09:01 -0300

    Custom theme

---
+++ src/components/themes/QatarTheme.tsx
@@ -0,0 +1,27 @@
+import { createTheme } from '@mui/material/styles';
+
+const QatarTheme = createTheme(
+    {
+        palette: {
+            mode: 'dark',
+            primary: {
+                main: '#9a1032',
+            },
+            secondary: {
+                main: '#07c7cc',
+            },
+            background: {
+                default: '#3e0315',
+                paper: '#880c31',
+            },
+            info: {
+                main: '#07c7cc',
+            },
+        },
+        typography: {
+            fontFamily: 'Poppins',
+        },
+    }
+)
+
+export default QatarTheme;
---
+++ src/context/ProdeContext.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, createContext } from 'react';
+
+export const AppCtx = createContext({});
+
+interface Props {
+    children: React.ReactNode
+}
+
+export const ProdeContext = ({ children }: Props) => {
+    
+}
+
+export default ProdeContext;
